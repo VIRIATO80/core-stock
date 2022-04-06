a8e5eefc12fc448928957e0d0692db7609f1dea6
Javier Lindo <javi.lindo@gmail.com>
2022-04-06 10:46:16 +0200

    Adding Entity new module
    Added:
    - New module entity
    - Stock entity
    - StockedShoe entity
    - Stock repository
    - Unit tests

---
+++ entity/src/main/java/com/example/demo/entity/Stock.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "stocks")
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private State state;
+
+    @OneToMany
+    private List<StockedShoe> shoes;
+
+
+    public enum State{
+
+        EMPTY,
+        FULL,
+        SOME
+    }
+
+}
---
+++ entity/src/main/java/com/example/demo/entity/StockedShoe.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "shoes")
+@IdClass(StockedShoeId.class)
+public class StockedShoe {
+    @Id
+    private BigInteger size;
+    @Id
+    private Color color;
+    private Integer quantity;
+
+
+    public enum Color {
+        BLACK,
+        BLUE
+    }
+}
